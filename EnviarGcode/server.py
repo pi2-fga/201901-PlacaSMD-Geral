15758d52d3fef61e08ebf54cefa356a7fc16ec6b
Gustavo Moreira <gustavomn93@gmail.com>
2019-07-08 21:48:37 -0300

    Finalizando gcode

--- EnviarGcode/server.py
+++ EnviarGcode/server.py
@@ -12,25 +12,38 @@ def getTime():
 
 @eel.expose    
 def enviarGcode(gcode):
-	s = serial.Serial('/dev/ttyACM0',9600)
+	s = serial.Serial('/dev/ttyUSB0',9600,timeout=5)
+	time.sleep(2) 
+	#s = serial.Serial('/dev/ttyACM0',9600)
 	print ( 'Abrindo porta serial' )
 	
 	f = open(gcode,'r') 
 	print ( 'Abrindo gcode' )   
-	s.write(("\r\n\r\n").encode('utf-8')) # Hit enter a few times to wake the Printrbot
 	time.sleep(3)   					  # Wait for Printrbot to initialize
-	s.flushInput()  					  # Flush startup text in serial input
+	s.flushInput()
+	s.flushOutput()  					  # Flush startup text in serial input
 	print ( 'Enviando gcode' )
 	
 	for line in f:
 		l = line
-		l = l.strip() 					  # Strip all EOL characters for streaming
-		if  (l.isspace()==False and len(l)>0) :
-			print ( 'Enviando: ' + l )
-			s.write(l) 					  # Send g-code block
-			grbl_out = s.read(10) 		  # Wait for response with carriage return
-			print ( ' : ' + grbl_out.strip() )
-		 
+		l = l.rstrip('\r\n')					  # Strip all EOL characters for streaming
+		if  (len(l)>0) :
+			for b in l: 								
+				d = bytearray(b'b')
+				s.write(b)
+				s.flush()
+				s.flushInput()
+				s.flushOutput() 
+					
+		s.flush() 
+		grbl_out = s.readline()
+		while(b'done' not in grbl_out): 
+			grbl_out = s.readline()
+			time.sleep(2)
+			print ( ' : ' + grbl_out )
+			
+		s.flushInput()
+		s.flushOutput()
 	
 	# Close file and serial port
 	f.close()
